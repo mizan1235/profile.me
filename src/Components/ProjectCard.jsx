f01611934ad59f694a60d0d1973ceca37d84f3de
mizan1235 <127872547+mizan1235@users.noreply.github.com>
2024-01-18 10:37:12 +0530

    Update ProjectCard.jsx

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -68,7 +68,7 @@ const ProjectCard = () => {
           <h2>Weather</h2>
         <div className='image-filed'>
             <a href="https://mizan1235.github.io/weather_forcast/" target='_blank' rel="noreferrer">
-          <img src="../../profile.me/Images/weather.png" alt="Weather" /></a>
+          <img src="../../profile.me/Images/weather_forcast.png" alt="Weather" /></a>
           </div>
           
           
